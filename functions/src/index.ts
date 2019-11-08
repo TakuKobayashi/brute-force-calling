e4e5415bd9a1126bd140b8b5c9faf7bcbd07f568
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-11-08 22:56:09 +0900

    電話とSMSを送れるように

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,25 @@
-import * as functions from 'firebase-functions';
+import {region, Request, Response} from "firebase-functions";
 
 const accountSid = 'ACe6324e501c8e4dc25a1f7f1a5351a1a3';
 const authToken = 'c2896e441f27b31849daf4daa4bf75f0';
-require('twilio')(accountSid, authToken);
+const client = require('twilio')(accountSid, authToken);
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
+export const sendMessage = region("asia-northeast1").https.onRequest((request: Request, response: Response) => {
+  // tslint:disable-next-line:no-shadowed-variable
+  const sendMessage = 'Hello from Node';
+  client.messages.create({
+    body: sendMessage,
+    to: '+818055146460',
+    from: '+17164076874',
+  }).then((message: any) => console.log(message));
+  response.send(sendMessage);
+});
+
+export const callPhone = region("asia-northeast1").https.onRequest((request: Request, response: Response) => {
+  client.calls.create({
+    url: 'http://demo.twilio.com/docs/voice.xml',
+    to: '+818055146460',
+    from: '+17164076874',
+  }).then((call: any) => console.log(call));
   response.send("Hello from Firebase!");
 });
