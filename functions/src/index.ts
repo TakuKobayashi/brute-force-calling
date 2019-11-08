4ace5f8f4b296ef6f6283bc3a93b9641d8ad8ac8
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-11-09 08:26:08 +0900

    多分あっていると思うConfigをセットしておいた

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,19 @@
-import {region, Request, Response} from "firebase-functions";
+import {region, Request, Response, config} from "firebase-functions";
 
+/*
+const functionConfig = config();
+console.log(functionConfig);
+console.log(process.env);
+const client = require('twilio')(functionConfig.twilio.account_sid, functionConfig.twilio.auth_token);
+*/
 const accountSid = 'ACe6324e501c8e4dc25a1f7f1a5351a1a3';
 const authToken = 'c2896e441f27b31849daf4daa4bf75f0';
 const client = require('twilio')(accountSid, authToken);
+
+
+export const settingConfig = region("asia-northeast1").https.onRequest((request: Request, response: Response) => {
+  response.json(config());
+});
 
 export const sendMessage = region("asia-northeast1").https.onRequest((request: Request, response: Response) => {
   // tslint:disable-next-line:no-shadowed-variable
@@ -11,6 +22,7 @@ export const sendMessage = region("asia-northeast1").https.onRequest((request: R
     body: sendMessage,
     to: '+818055146460',
     from: '+17164076874',
+//    from: functionConfig.phone_number.us_sms,
   }).then((message: any) => console.log(message));
   response.send(sendMessage);
 });
@@ -20,6 +32,7 @@ export const callPhone = region("asia-northeast1").https.onRequest((request: Req
     url: 'http://demo.twilio.com/docs/voice.xml',
     to: '+818055146460',
     from: '+17164076874',
+//    from: functionConfig.phone_number.jp_voice,
   }).then((call: any) => console.log(call));
   response.send("Hello from Firebase!");
 });
