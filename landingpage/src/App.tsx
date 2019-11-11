3be06d906d7da0f394fbb91a8919dcdbd83bdcc2
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-11-12 06:58:02 +0900

    文言修正

--- landingpage/src/App.tsx
+++ landingpage/src/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          電話をかけてみてね!!
+          この番号に電話をかけてみてね!!
         </p>
         <a
           className="App-link"
